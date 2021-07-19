d4cceafbe6c3e99bd3c274d5846b1955b609f8cf
Rebellious83 <rafal.templar@gmail.com>
2021-07-19 12:55:03 +0200

    global TimeoutInterceptor added

--- src/interfaces/character-document.interface.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable prettier/prettier */
-import { Document } from 'mongoose';
-export interface CharacterDoc extends Document {
-  name: string;
-  episodes: Array<string>;
-  planet: string;
-}
